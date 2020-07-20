f5ba4789997fc0c584fdacc48d2adf2ce9729c5a
anakinliu <anakinliu@tencent.com>
2020-07-20 15:08:43 +0800

    init

---
+++ .brew/swiftlint.rb
@@ -0,0 +1,24 @@
+class Swiftlint < Formula
+  desc "Tool to enforce Swift style and conventions"
+  homepage "https://github.com/realm/SwiftLint"
+  url "https://github.com/realm/SwiftLint.git",
+      :tag      => "0.39.2",
+      :revision => "b1c72069cafa6cdebfb9274806cd976c54d8dde5"
+  head "https://github.com/realm/SwiftLint.git"
+
+  depends_on :xcode => ["10.2", :build]
+  depends_on :xcode => "8.0"
+
+  def install
+    system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SwiftLint.dst"
+  end
+
+  test do
+    (testpath/"Test.swift").write "import Foundation"
+    assert_match "Test.swift:1:1: warning: Trailing Newline Violation: " \
+                 "Files should have a single trailing newline. (trailing_newline)",
+      shell_output("SWIFTLINT_SWIFT_VERSION=3 SWIFTLINT_DISABLE_SOURCEKIT=1 #{bin}/swiftlint lint --no-cache").chomp
+    assert_match version.to_s,
+      shell_output("#{bin}/swiftlint version").chomp
+  end
+end
